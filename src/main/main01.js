52d4839c716b0842cc4820530a40ebc566e54044
邱彬华 <43344531+qbh990803@users.noreply.github.com>
2022-11-24 19:39:21 +0800

    feat: 轨道控制器查看物体

---
+++ src/main/main01.js
@@ -0,0 +1,53 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+console.log("[ THREE ] >", THREE);
+
+// 使用控制器查看3d物体
+
+// 创建场景
+const scene = new THREE.Scene();
+
+// 创建相机
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+// 设置相机位置
+camera.position.set(0, 0, 10);
+scene.add(camera);
+
+// 添加物体
+// 创建几何体
+const cubeGeometry = new THREE.BoxGeometry();
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffff00,
+});
+// 根据几何体和材质创建物体
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+// 将几何体添加到场景当中
+scene.add(cube);
+
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer();
+// 设置渲染的尺寸大小
+renderer.setSize(window.innerWidth, window.innerHeight);
+console.log("renderer", renderer);
+
+// 将webgl渲染的canvas添加到body
+document.body.appendChild(renderer.domElement);
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function render() {
+  // 使用渲染器，通过相机将场景渲染进来
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(render);
+}
+
+render();
